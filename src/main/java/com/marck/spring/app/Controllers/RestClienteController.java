104a05e991c343d4ad625b0b6e270848a428bc8e
Marco Antonio Rosas Ibarra <rimazer0.2013@gmail.com>
2017-12-15 11:12:24 -0600

    Actualizacion de Buscar con Ajax

--- src/main/java/com/marck/spring/app/Controllers/RestClienteController.java
+++ src/main/java/com/marck/spring/app/Controllers/RestClienteController.java
@@ -1,10 +1,13 @@
 package com.marck.spring.app.Controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,22 +15,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marck.spring.app.Models.CimplS;
 import com.marck.spring.app.Models.Response;
+import com.marck.spring.app.Models.Entity.Buscar;
 import com.marck.spring.app.Models.Entity.Cliente;
 
 @RestController
-@RequestMapping("cliente/api/cliente")
+@RequestMapping("cliente/")
 public class RestClienteController {
 	@Autowired
 	CimplS cs;
 	
-	List<Cliente> cli= new ArrayList<Cliente>();
-	Cliente clione= new Cliente();
+	
 	@PostMapping(value="/buscar")
-	public Response getResource(@RequestBody String valor) {
-		
-		clione=cs.findOne(Long.parseLong(valor));
+	public Response getResource(@RequestBody String valor) throws JsonParseException, JsonMappingException, IOException {
+		List<Cliente> cli= new ArrayList<Cliente>();
+		Cliente clione= new Cliente();
+		Buscar ob = new ObjectMapper().readValue(valor, Buscar.class);
+		clione=cs.findOne(Long.parseLong(ob.getBuscar()));
 		cli.add(clione);
 		System.out.println(cli);
 		Response response= new Response("Done",cli);
---
+++ src/main/java/com/marck/spring/app/Models/Entity/Buscar.java
@@ -0,0 +1,20 @@
+package com.marck.spring.app.Models.Entity;
+
+
+
+public class Buscar{
+	/**
+	 * 
+	 */
+	
+	private String buscar;
+
+	public String getBuscar() {
+		return buscar;
+	}
+
+	public void setBuscar(String buscar) {
+		this.buscar = buscar;
+	}
+	
+}
